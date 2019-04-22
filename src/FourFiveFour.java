9cbac8f9de0ff4498af34af7ef0cadd5519bfef5
fzh <15521206029@163.com>
2019-04-22 11:33:55 +0800

    更新454题

--- src/FourFiveFour.java
+++ src/FourFiveFour.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class FourFiveFour {
     public static void main(String[] args) {
@@ -6,12 +7,14 @@ public class FourFiveFour {
         int [] B = {-1,1,0};
         int [] C = {0,0,1};
         int [] D = {-1,1,1};
-        System.out.println(fourSumCount(A, B, C, D));
+        System.out.println(fourSumCount2(A, B, C, D));
     }
 
     /**
      * 给定四个包含整数的数组列表 A , B , C , D ,计算有多少个元组 (i, j, k, l) ，使得 A[i] + B[j] + C[k] + D[l] = 0。
      * 注意：元组元素不能重复
+     *
+     * 超出时间限制
      *
      * @param A
      * @param B
@@ -30,38 +33,67 @@ public class FourFiveFour {
         Arrays.sort(B);
         Arrays.sort(C);
         Arrays.sort(D);
+        for (int i = 0; i < C.length; i++) {
+            System.out.print(C[i] + "  ");
+        }
+        System.out.println();
+        for (int i = 0; i < D.length; i++) {
+            System.out.print(D[i] + "  ");
+        }
+        System.out.println();
         int result = 0;
         //先固定两个数A[i]和B[i]，再确定另外两个数
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
+//                System.out.println("i = " + i + ", j = " + j);
                 int sum = A[i] + B[j];
-                if (0 - sum < C[0] + D[0]) {    //若C、D集合中的第一个数加起来就大过0，无需再遍历
+                if (0  < C[0] + D[0] + sum) {    //若C、D集合中的第一个数加起来就大过0，无需再遍历
                     break;
                 }
-                int l = 0;
-                int r = n-1;
-                while (l < n && r >= 0) {
-                    int sum2 = C[l] + D[r];
-                    if (sum + sum2 == 0) {
-                        result++;
-                        int l1 = 1;
-                        l++;
-                        while (l < n && C[l] == C[l-1]) {
-                            l1++;
-                            l++;
+                for (int k = 0; k < n; k++) {
+                    for (int l = 0; l < n; l++) {
+                        int sum2 = C[k] + D[l];
+                        if (sum2 + sum > 0) {
+                            break;
+                        } else if (sum2 + sum == 0) {
+//                            System.out.println("i = " + i + ", j = " + j + ", k = " + k + ", l = " + l);
+                            result++;
                         }
-                        int r1 = 1;
-                        r--;
-                        while (r >=0 && C[r] == C[r+1]) {
-                            r1++;
-                            r--;
-                        }
-                        result += l1 * r1 - 1;
-                    } else if (sum + sum2 > 0) {
-                        r--;
-                    } else {
-                        l++;
                     }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * 借助HashMap先存储A[i] + B[j]的值，再比较C[k] + C[l]
+     */
+    private static int fourSumCount2(int[] A, int[] B, int[] C, int[] D) {
+        int n = A.length;
+        if (n < 1) {
+            return 0;
+        }
+
+        HashMap<Integer, Integer> hashMap = new HashMap<>();    //key为A[i] + B[j]的值，value为该值的组合个数
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int key = A[i] + B[j];
+                if (hashMap.containsKey(key)) {
+                    hashMap.put(key, hashMap.get(key) + 1);
+                } else {
+                    hashMap.put(key, 1);
+                }
+            }
+        }
+
+        for (int k = 0; k < n; k++) {
+            for (int l = 0; l < n; l++) {
+                int need = - (C[k] + D[l]);  //加起来为0所需要的key
+                if (hashMap.containsKey(need)) {
+                    result += hashMap.get(need);
                 }
             }
         }
