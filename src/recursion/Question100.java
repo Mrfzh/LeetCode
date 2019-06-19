2871e7ea02b0f5e5cdc48db71afb35e151d62c3d
fzh <15521206029@163.com>
2019-06-19 08:52:48 +0800

    增加第100题

---
+++ src/recursion/Question100.java
@@ -0,0 +1,33 @@
+package recursion;
+
+public class Question100 {
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 给定两个二叉树，编写一个函数来检验它们是否相同。
+     * 如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的。
+     *
+     * 递归
+     *
+     * @param p
+     * @param q
+     * @return
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null) {
+            return false;
+        } else if (q == null) {
+            return false;
+        }
+
+        if (p.val != q.val) {
+            return false;
+        } else {
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+}
